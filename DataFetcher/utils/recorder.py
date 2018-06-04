572a23a89a49d2ff412e341a72ced03149be2245
Gibson <natsumi-0416@hotmail.com>
2018-06-04 20:25:02 +0800

    [DataFetcher] fix os mkdir failed with not found path

--- DataFetcher/utils/recorder.py
+++ DataFetcher/utils/recorder.py
@@ -7,7 +7,7 @@ class Recorder(object):
     def __init__(self, path):
         self.folder_path = '{}/{}'.format(path, date.today().strftime('%Y%m%d'))
         if not os.path.isdir(self.folder_path):
-            os.mkdir(self.folder_path)
+            os.makedirs(self.folder_path)
 
     # Child classes can override this method
     def record_to_csv(self, data):
