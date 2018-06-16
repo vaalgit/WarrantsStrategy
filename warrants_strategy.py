6e190b5252702d7259127c49753328bcae3b5b75
vaalgit <natsumi-0416@hotmail.com>
2018-06-16 23:26:36 +0800

    [main] remove minor docstring

--- warrants_strategy.py
+++ warrants_strategy.py
@@ -1,15 +1,12 @@
 import os
 from DataFetcher.crawl import main as do_parse
 
-""" This script for execute warrants strategy! """
 CURRENT_PATH = os.getcwd()
 def main():
-    """ This script for execute warrants strategy! """
     do_parse(
         input_path=os.path.join(CURRENT_PATH, 'stocknumber.csv'),
         output_path=os.path.join(CURRENT_PATH, 'Result/data'))
 
 
 if __name__ == '__main__':
-    """ This script for execute warrants strategy! """
     main()
